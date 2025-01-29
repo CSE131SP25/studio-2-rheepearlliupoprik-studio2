7c9eb331faa6f9c308e8cbb85e8282c62ecae82a
liuyi <liuyi@DESKTOP-IBQSS4Q.dhcp.wustl.edu>
2025-01-29 15:49:59 -0600

    1

---
+++ src/studio2/Ruin.java
@@ -0,0 +1,45 @@
+package studio2;
+
+import java.util.Scanner;
+public class Ruin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner in = new Scanner(System.in);
+		System.out.println("Your start amount");
+		double startAmount = in.nextDouble();
+		System.out.println("Win chance");
+		double winchance = in.nextDouble();
+		System.out.println("win limit");
+		double winlimit = in.nextDouble();
+		System.out.println("Total simultaions: " ); 
+	    int totalSimulations = in.nextInt();
+		 
+	    double j = startAmount;
+	    
+	    
+	    while(j < winlimit&&j>0) {
+			double k = Math.random();
+			if(k>winchance) {
+				System.out.println("Success");
+			    j++;}
+			else if(k<winchance) {
+				System.out.println("Ruin");
+				j--;
+			System.out.println(j);
+			
+		}
+	    
+		//for(int i = totalSimulations; i<= 0; i++) {
+			//while(j < winlimit||j>0) {
+				//double k = Math.random();
+				//if(k>winchance)
+				  //  j++;
+				//else if(k<winchance)
+					//j--;
+				//System.out.println(j);
+			}
+		}
+	}
+ 
